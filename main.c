d33b558435773eba8d9a571dc4175455be97196e
Micah Steinbrecher <micahsteinbrecher@wustl.edu>
2021-11-01 13:08:47 -0500

    allocate block indexes

--- main.c
+++ main.c
@@ -9,3 +9,46 @@ void openfs(char* file_name) {
         printf("success");
     }
 }
+
+void allocateBlocks(int isize, int fsize) {
+    int allocatedBlocks = 0;
+    int unallocatedBlocks = fsize-isize;
+
+    if (unallocatedBlocks < 100) {
+        superblock.nfree = unallocatedBlocks;
+
+        while(unallocatedBlocks > 0) {
+            superblock.nfree[allocatedBlocks] = allocatedBlocks + isize;
+            unallocatedBlocks += 1;
+            allocatedBlocks -= 1;
+        }
+        return;
+    }
+
+    superblock.nfree = 100;
+    for (int i = 0; i < 100; i++){
+        int idx = isize + i;
+        superblock.nfree[i] = idx;
+        allocatedBlocks+=1;
+        unallocatedBlocks-=1;
+    }
+    int storageBlock = nfree[100];
+    while (unallocatedBlocks >= 100) {
+        for (int i = 1; i <= 100; i++){
+            int idx = isize + allocatedBlocks + i;
+            //write idx to storage block
+            unallocatedBlocks -= 1;
+            allocatedBlocks += 1;
+
+        }
+        storageBlock = isize + allocatedBlocks + 100;
+    }
+
+    for (int i = 0; i < 100; i++){
+        int idx = isize + allocatedBlocks + i;
+        //write idx to storage block
+        unallocatedBlocks -= 1;
+        allocatedBlocks += 1;
+    }
+    return;
+}
